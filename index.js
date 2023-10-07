9a48d3ce724e8bf3f4e7f12939c4fd400a11c713
Tony Glass <idttip.tg@gmail.com>
2023-10-07 14:38:44 -0700

    completed First- Class Functions Lab

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = (ScubersDrivers) => {
+    return[ScubersDrivers[0], ScubersDrivers[1]]
+}
+
+const returnLastTwoDrivers = (ScubersDrivers) => {
+    return[ScubersDrivers[2], ScubersDrivers[3]]
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+const createFareMultiplier = (Number) => {
+    return function(fare) {
+        return fare * Number
+    }
+}
+
+const fareDoubler = (fare) => {
+    return fare * 2
+}
+
+const fareTripler = (fare) => {
+    return fare * 3
+}
+
+const selectDifferentDrivers = (drivers, callback) => {
+    return callback (drivers)
+
+}
